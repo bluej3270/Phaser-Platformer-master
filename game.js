34dd624ef36d149ae042594a1b61b4eaa5271a54
bluej3270 <kellanclapp@gmail.com>
2020-08-20 17:02:14 -0600

    -

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ function preload() {
 	this.load.image('bug3', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_3.png');
 	this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png');
 	this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/codey.png');
-	this.load.image('star', "https://opengameart.org/sites/default/files/star_4.png");
+	this.load.image('star', "assets/star.png");
 }
 
 function create() {
